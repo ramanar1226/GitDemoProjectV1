bb520377881121cdaa9b7b6dab3e3239c180d442
DELL <DELL@DESKTOP-3SEOMIF>
2020-03-10 08:16:14 +0530

    test2 file addd

--- src/Test2.java
+++ src/Test2.java
@@ -1,8 +1,11 @@
 
-public class Test1 {
+public class Test2 {
 	public static void main(String[] args) {
 		for (int i = 0; i < 10; i++) {
-			//for loop changed
+			//for loop
+			System.out.println("value is :  " + i);
+			
+			//for loop
 			System.out.println("value is :  " + i);
 		}
 	}
