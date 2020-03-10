f4f7dd75c553dcb8109dbb8006d15f1a58dcf163
ramanar1226 <40364554+ramanar1226@users.noreply.github.com>
2020-03-10 08:21:27 +0530

    comment modified

--- src/Test1.java
+++ src/Test1.java
@@ -2,7 +2,7 @@
 public class Test1 {
 	public static void main(String[] args) {
 		for (int i = 0; i < 10; i++) {
-			//for loop changed
+			//for loop changed in test1
 			System.out.println("value is :  " + i);
 		}
 	}
